6ac144a2f2e919a42de9c6bc690887b7bc2d315b
tomsembl <94670548+tomsembl@users.noreply.github.com>
2023-01-25 20:46:06 +0800

    Update 23.3.py

--- 2021/23.3.py
+++ 2021/23.3.py
@@ -10,46 +10,33 @@ a="""#############
 ###A#D#B#D###
   #B#C#A#C#
   #########"""
-test="""#############
-#...........#
-###B#C#B#D###
-  #A#D#C#B#
-  #A#D#C#B#
-  #A#D#C#A#
-  #########"""
+
 test="""#############
 #...........#
 ###B#C#B#D###
   #A#D#C#A#
   #########"""
 #a=test
+part2Mid="""  #D#C#B#A#
+  #D#B#A#C#"""
+a="\n".join(a.splitlines()[:3]+[part2Mid]+a.splitlines()[3:]) #toggle for part 1 / 2
 alphToNum = {"A":1,"B":2,"C":3,"D":4}
 boxStateSplit=[[alphToNum[a.splitlines()[y][x]] for y in range(len(a.splitlines())-2,1,-1)] for x in range(3,10,2)]
 h=len(boxStateSplit[0])
+h4=h*4
 boxState = [y for x in boxStateSplit for y in x]
 hallState = [0,0,0,0,0,0,0]
-graph = {i*h+j:{x:1 for x in [i-1,i+1] if j*h<=x<(j*h+4)} for i in range(h) for j in range(4) }
-print(graph)
-graph={
-    0:{1:1},
-    1:{0:1,9:2,10:2},
-    2:{3:1},
-    3:{2:1,10:2,11:2},
-    4:{5:1},
-    5:{4:1,11:2,12:2},
-    6:{7:1},
-    7:{6:1,12:2,13:2},
-    8:{9:1},
-    9:{8:1,10:2,1:2},
-    10:{9:2,11:2, h-1:2, 2*h-1:2},
-    11:{10:2,12:2, 2*h-1:2, 3*h-1:2},
-    12:{11:2,13:2, 3*h-1:2, 4*h-1:2},
-    13:{12:2,14:1, 4*h-1:2},
-    14:{13:1},
-}
+graph = {(y:=j*h+i):{x:1 for x in [y-1,y+1] if j*h<=x<(j*h+h)}  for j in range(4) for i in range(h) } #room->room neighbours
+[ graph[i*h+h-1].update( {x:2 for x in [h4+1+i,h4+i+2]}) for i in range(4) ] #room->hall neighbours
+graph.update({ h4+i:{ x:(1 if all([z in [h4+0,h4+1,h4+5,h4+6] for z in [x,h4+i]]) else 2) for x in [h4+i-1,h4+i+1] if h4<=x<h4+7} for i in range(7) }) #hall->hall neighbours
+[ graph[h4+i].update({x*h-1:2 for x in [i-1,i] if 0<x and x<5}) for i in range(7) ] #hall->room neighbours
+
+for x in sorted(graph):
+    print(x,graph[x])
+print()
+
 global distances
 distances={}
-distancesHome2Home={}
 def bfs(start):
     queue=[[start,[],0]]
     visited = set()
@@ -58,35 +45,27 @@ def bfs(start):
         if node not in visited:
             visited.add(node)
             if node != start:
-                if ((start<8) != (node<8)):
+                if ((start<h4) != (node<h4)) or start<h4:
                     distances[start][node] = [path,cost]
-                elif start<8:
-                    distancesHome2Home[start][node] = [path,cost]
             for neighbour,cost2 in graph[node].items():
                 queue.append([neighbour,path+[neighbour],cost+cost2])
 
 
 for start in range(len(graph)):
     distances[start] = {}
-    distancesHome2Home[start] = {}
     bfs(start)
     for destination in distances[start]:
         print(start,"->",destination, distances[start][destination])
-    for destination in distancesHome2Home[start]:
-        print(start,"->",destination, distancesHome2Home[start][destination])
 
-endState=[1,1,2,2,3,3,4,4]
-def validateHome(boxState,i,amphipod):
-    if endState[i] == amphipod: 
-        if i in [1,3,5,7]:
-            return boxState[i-1] == amphipod
-    return False
+endState=[x for x in range(1,5) for _ in range(h) ]
+def validateHome(boxState,j):
+    return not any([True for x in boxState[(j-1)*h:j*h] if x not in [0,j]])
 
 def getHome(boxState,amphipod):
-    homeBottomLocation = 2*amphipod-2
-    homeBottomContents = boxState[homeBottomLocation]
-    if homeBottomContents==0: return homeBottomLocation
-    if homeBottomContents==amphipod: return homeBottomLocation+1
+    homeBottom = (amphipod-1)*h
+    for x in range(h):
+        if boxState[homeBottom+x] == 0: return homeBottom+x
+        if boxState[homeBottom+x] != amphipod: return -1
     return -1
 
 def validatePath(state,path):
@@ -111,8 +90,7 @@ def cacheCheck(boxState,hallState,energy,returnHit=False):
     return True
 
 costs=[0,1,10,100,1000]
-costMultiplier = lambda amphipod: costs[amphipod]
-minEnergy=999999999999
+minEnergy=99999
 queue=[[boxState,hallState,0]]
 cache={}
 iterations=0
@@ -129,33 +107,38 @@ while True:
         continue
 
     state = boxState + hallState
-    for i,amphipod in enumerate(boxState):
+    for j in range(1,5):
+        i=j*h-1
+        while boxState[i]==0 and i>=0: 
+            i-=1
+        if i<0: continue
+        amphipod = boxState[i]
         if amphipod == 0: continue
-        if not validateHome(boxState, i, amphipod):
+        if not validateHome(boxState, j):
 
             #go home -> hall
             possibleDestinations = []
-            for x in distances[i][8][0]: #left side of hall
+            for x in distances[i][h4][0]: #left side of hall
                 if validateNode(state,x):
-                    if x>7: possibleDestinations.append(x)
+                    if x>h4-1: possibleDestinations.append(x)
                 else: break
-            for x in distances[i][14][0]: #right side of hall
+            for x in distances[i][h4+6][0]: #right side of hall
                 if validateNode(state,x):
-                    if x>7: possibleDestinations.append(x)
+                    if x>h4-1: possibleDestinations.append(x)
                 else: break
             for x in possibleDestinations:
                 path, cost = distances[i][x]
-                newQueueItem = [boxState[:i]+[0]+boxState[i+1:], hallState[:x-8]+[amphipod]+hallState[x-7:], energy+cost*costMultiplier(amphipod)]
+                newQueueItem = [boxState[:i]+[0]+boxState[i+1:], hallState[:x-h4]+[amphipod]+hallState[x-h4+1:], energy+cost*costs[amphipod]]
                 if cacheCheck(*newQueueItem): queue.append(newQueueItem)
 
             #go home -> home
             if (home := getHome(boxState,amphipod)) == -1: continue
-            path, cost = distancesHome2Home[i][home]
+            path, cost = distances[i][home]
             if validatePath(state,path):
                 newBoxState = boxState[:]
                 newBoxState[i]=0
                 newBoxState[home]=amphipod
-                newQueueItem = [newBoxState, hallState, energy+cost*costMultiplier(amphipod)]
+                newQueueItem = [newBoxState, hallState, energy+cost*costs[amphipod]]
                 if cacheCheck(*newQueueItem): queue.append(newQueueItem)
 
     for i2,amphipod2 in enumerate(hallState):
@@ -163,10 +146,9 @@ while True:
         if (home := getHome(boxState,amphipod2)) == -1: continue
 
         #go hall -> home
-        path, cost = distances[i2+8][home]
+        path, cost = distances[i2+h4][home]
         if validatePath(state,path):
-            newQueueItem = [boxState[:home]+[amphipod2]+boxState[home+1:], hallState[:i2]+[0]+hallState[i2+1:], energy+cost*costMultiplier(amphipod2)]
+            newQueueItem = [boxState[:home]+[amphipod2]+boxState[home+1:], hallState[:i2]+[0]+hallState[i2+1:], energy+cost*costs[amphipod2]]
             if cacheCheck(*newQueueItem): queue.append(newQueueItem)
 
-print("Minimum Energy:", minEnergy) #part 1
-#print("cacheSize:",sum([len(cache[x]) for x in cache]))
+print("Minimum Energy:", minEnergy) #part 2
