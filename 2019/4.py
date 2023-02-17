ba6cd163b1928112df37f888aea5c26846e5548a
tomsembl <94670548+tomsembl@users.noreply.github.com>
2023-02-17 19:49:35 +0800

    a

--- 2019/4.py
+++ 2019/4.py
@@ -7,10 +7,30 @@ def check(x):
         if y==last and i!=0: double=True
         last=y
     return double
+total=0
+for x in range(108457,562041+1):
+    if check(x):
+        total+=1
+print(total)#part1
+
+
+def check(x):
+    x=str(x)
+    last=x[0]
+    groups={}
+    for i,y in enumerate(x):
+        if y<last: return False
+        if y not in groups: groups[y]=[]
+        groups[y].append(i)
+        last=y
+    consecutive=False
+    for val,poses in groups.items():
+        if len(poses)==2 and abs(poses[0]-poses[1])==1: consecutive=True
+    return consecutive
 
 total=0
 for x in range(108457,562041+1):
     if check(x):
         total+=1
 print(total)
-#2863 too high
+#1444 too low
