ab1c56efbd711dc30ea8d1cc51beb9b0a3539284
tomsembl <94670548+tomsembl@users.noreply.github.com>
2023-08-30 17:42:24 +0800

    a

--- 2017/13.py
+++ 2017/13.py
@@ -1,4 +1,74 @@
-a=""""""
-test=""""""
-a=test
-b=[x for x in a.splitlines()]
+a="""0: 3
+1: 2
+2: 4
+4: 4
+6: 5
+8: 6
+10: 6
+12: 8
+14: 6
+16: 6
+18: 9
+20: 8
+22: 8
+24: 8
+26: 12
+28: 8
+30: 12
+32: 12
+34: 12
+36: 10
+38: 14
+40: 12
+42: 10
+44: 8
+46: 12
+48: 14
+50: 12
+52: 14
+54: 14
+56: 14
+58: 12
+62: 14
+64: 12
+66: 12
+68: 14
+70: 14
+72: 14
+74: 17
+76: 14
+78: 18
+84: 14
+90: 20
+92: 14"""
+test="""0: 3
+1: 2
+4: 4
+6: 4"""
+#a=test
+b={int(y):int(z) for y,z in  [x.split(": ") for x in a.splitlines()]}
+listB = sorted(list(b))
+
+severity = 0
+for time in listB:
+    range_ = b[time]
+    scannerPos = time % (range_*2-2)
+    if scannerPos == 0:
+        severity += time*range_
+print(severity)#part 1
+
+exclusions = {time:b[time]*2-2 for time in b if b[time] != 0}
+delay = 0
+while True:
+    delay += 1
+    breaker = False
+    for time in exclusions:
+        exclusion  = exclusions[time]
+        totalTime = time+delay
+        if totalTime % exclusion == 0:
+            breaker = True
+            break
+    if not breaker: 
+        print(delay)#part 2
+        break
+    
--- 2017/14.py
+++ 2017/14.py
@@ -1,4 +1,68 @@
-a=""""""
-test=""""""
-a=test
-b=[x for x in a.splitlines()]
+a="ugkiagan"
+test="flqrgnkx"
+#a=test
+size=128
+rows=[f"{a}-{i}" for i in range(size)]
+
+def knotHash(a):
+    b=[ord(x) for x in a] + [17, 31, 73, 47, 23]
+    size=5
+    size = 256
+    c = list(range(size))
+    pos = 0
+    skip = 0
+    for round in range(64):
+        for length in b:
+            slice = (c*2)[pos:pos+length]
+            endPos = (pos+length)%size
+            antiSlice = c[endPos:]+c[:pos] if endPos >= pos else c[endPos:pos]
+            c = slice[::-1] + antiSlice
+            c = (c*2)[size-pos:2*size-pos]
+            pos += length + skip
+            pos %= size
+            skip += 1
+    sparseHash = c
+    denseSize = 16
+    denseHash = []
+    for i in range(denseSize):
+        string1 = str(sparseHash[i*denseSize : (i+1)*denseSize])[1:-1].replace(", ","^")
+        denseHash.append(eval(string1))
+    denseHashHex = "".join([hex(x)[2:].zfill(2) for x in denseHash])
+    return denseHashHex
+
+def hexToBin(x): return bin(int(x,16))[2:].zfill(4)
+
+matrix = [[int(y) for y in "".join([hexToBin(x) for x in knotHash(row)])] for row in rows]
+print(sum([sum(row) for row in matrix])) #part 1
+
+def isUsed(c):
+    x,y = c
+    if 0 <= x < size and 0 <= y < size:
+        return bool(matrix[y][x])
+    return False
+
+
+groups = []
+for y in range(size):
+    for x in range(size):
+        if not isUsed((x,y)): continue
+        if any([(x,y) in group for group in groups]): continue
+        seen = set()
+        queue = [(x,y)]
+        while queue:
+            node = queue.pop(0)
+            seen.add(node)
+            nx,ny = node
+            for dx,dy in [(1,0),(0,-1),(0,1),(-1,0)]:
+                newNode = (nx+dx,ny+dy)
+                if isUsed(newNode) and newNode not in seen:
+                    queue.append(newNode)
+        if len(seen) > 0: groups.append(seen)
+print(len(groups))
+# matrix2 = [[str(x) for x in y] for y in matrix]
+# for group in groups:
+#     for x,y in group:
+#         matrix2[y][x] = "#"
+
+# for row in matrix2:
+#     print("".join(row)[:20].replace("0"," "))
