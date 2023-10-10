367557464e5d2c6b6e4f1af159859ad43d8ea671
tom.mcelhinney <antipants94@gmail.com>
2023-10-10 15:28:13 +0800

    Update 10.py

--- 2015/10.py
+++ 2015/10.py
@@ -13,10 +13,12 @@ def lookAndSay(x):
         count += 1
     out += str(count) + last
     return out
+
 def part(p):
     a="1321131112"
     for _ in range(40 if p==1 else 50):
         a=lookAndSay(a)
     print(len(a))
+    
 part(1)
 part(2)
