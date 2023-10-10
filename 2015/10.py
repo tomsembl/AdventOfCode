c1fbce1809d354b8f2dc0cf84f31e5e40c90057b
tom.mcelhinney <antipants94@gmail.com>
2023-10-10 15:22:35 +0800

    a

--- 2015/10.py
+++ 2015/10.py
@@ -1,4 +1,22 @@
-a=""""""
-test=""""""
-a=test
-b=[x for x in a.splitlines()]
+a="1321131112"
+test="111221"
+#a=test
+def lookAndSay(x):
+    last = x[0]
+    count=1
+    out = ""
+    for y in x[1:]:
+        if y!=last:
+            out += str(count) + last
+            count=0
+        last = y
+        count += 1
+    out += str(count) + last
+    return out
+def part(p):
+    a="1321131112"
+    for _ in range(40 if p==1 else 50):
+        a=lookAndSay(a)
+    print(len(a))
+part(1)
+part(2)
