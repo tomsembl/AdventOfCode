b4da8aad606f8d296403d1565d55fcc7f32a9736
tom.mcelhinney <antipants94@gmail.com>
2023-12-06 13:27:14 +0800

    a

--- 2023/6.1.py
+++ 2023/6.1.py
@@ -1,10 +1,13 @@
-#part 1 input
-times=[49,78,79,80]
-distances=[298,1185,1066,1181]
-
-#part 2 input
+a="""Time:        49     78     79     80
+Distance:   298   1185   1066   1181"""
+test="""Time:      7  15   30
+Distance:  9  40  200"""
 times=[49787980]
 distances=[298118510661181]
+
+#test
+# times=[7,15,30]
+# distances=[9,40,200]
 
 total = 1
 for i,t in enumerate(times):
