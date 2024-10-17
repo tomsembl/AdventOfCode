13ab6ab7965f82264074805b8a1916afdacfa162
tomsembl <94670548+tomsembl@users.noreply.github.com>
2024-10-17 10:40:18 +0800

    21.3

--- base64.py
+++
@@ -1,8 +0,0 @@
-import json
-import base64
-def decode64(a):
-    print(json.dumps(base64.b64decode(a).decode('ascii')))
-def encode64(b):
-    print(base64.b64encode(b.encode('ascii')).decode('ascii'))
-decode64("")
-encode64("")
--- sharepointList.py
+++
@@ -1,9 +0,0 @@
-
-from shareplum import Site
-from requests_ntlm import HttpNtlmAuth
-
-url = ""
-listName = "AllItems"
-
-auth = HttpNtlmAuth('username', 'password')
-site = Site(url, auth=auth)
