f9888ec25fdfdf5cfbf12108c0d3574d323dc171
tomsembl <94670548+tomsembl@users.noreply.github.com>
2024-12-24 17:33:10 +0800

    24

--- 2024/24.2.py
+++ 2024/24.2.py
@@ -311,85 +311,135 @@ sps AND fqr -> cwh
 svw OR dvg -> gdq
 y25 AND x25 -> mnt
 nwf OR fvp -> pwc"""
-test="""x00: 1
+test="""x00: 0
 x01: 1
-x02: 1
-y00: 0
-y01: 1
-y02: 0
-
-x00 AND y00 -> z00
-x01 XOR y01 -> z01
-x02 OR y02 -> z02"""
-test="""x00: 1
-x01: 0
-x02: 1
+x02: 0
 x03: 1
 x04: 0
-y00: 1
-y01: 1
+x05: 1
+y00: 0
+y01: 0
 y02: 1
 y03: 1
-y04: 1
+y04: 0
+y05: 1
 
-ntg XOR fgs -> mjb
-y02 OR x01 -> tnw
-kwq OR kpj -> z05
-x00 OR x03 -> fst
-tgd XOR rvg -> z01
-vdt OR tnw -> bfw
-bfw AND frj -> z10
-ffh OR nrd -> bqk
-y00 AND y03 -> djm
-y03 OR y00 -> psh
-bqk OR frj -> z08
-tnw OR fst -> frj
-gnj AND tgd -> z11
-bfw XOR mjb -> z00
-x03 OR x00 -> vdt
-gnj AND wpb -> z02
-x04 AND y00 -> kjc
-djm OR pbm -> qhw
-nrd AND vdt -> hwm
-kjc AND fst -> rvg
-y04 OR y02 -> fgs
-y01 AND x02 -> pbm
-ntg OR kjc -> kwq
-psh XOR fgs -> tgd
-qhw XOR tgd -> z09
-pbm OR djm -> kpj
-x03 XOR y03 -> ffh
-x00 XOR y04 -> ntg
-bfw OR bqk -> z06
-nrd XOR fgs -> wpb
-frj XOR qhw -> z04
-bqk OR frj -> z07
-y03 OR x01 -> nrd
-hwm AND bqk -> z03
-tgd XOR rvg -> z12
-tnw OR pbm -> gnj"""
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00"""
+test="""x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
+
+x00 AND y00 -> z00
+x01 AND y01 -> z01
+x02 AND y02 -> z02
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z05"""
 #a=test
 b={x.split(":")[0]:int(x.split(": ")[1]) for x in a.split("\n\n")[0].splitlines()}
 c=[x.split()+[False] for x in a.split("\n\n")[1].splitlines()]
+OPS = {"AND": lambda x,y: x&y, "OR": lambda x,y: x|y, "XOR": lambda x,y: x^y}
 
-print(b,c)
-while any([not x[-1] for x in c]):
-    for i,x in enumerate(c):
-        if x[-1]:
-            continue
-        if x[0] not in b or x[2] not in b:
-            continue
-        if x[1]=="AND":
-            b[x[4]]=b[x[0]]&b[x[2]]
-        if x[1]=="OR":
-            b[x[4]]=b[x[0]]|b[x[2]]
-        if x[1]=="XOR":
-            b[x[4]]=b[x[0]]^b[x[2]]
-        c[i][-1]=True
+parents = {}
+for x in c:
+    parents[x[4]] = [x[0],x[2],x[1]]
+    
+xStr = ""
+for x in sorted([x for x in b if x.startswith("x")]):
+    xStr = str(b[x]) + xStr
+    
+yStr = ""
+for x in sorted([x for x in b if x.startswith("y")]):
+    yStr = str(b[x]) + yStr
 
-total = 0
-for x in sorted([x for x in b if x.startswith("z")]):
-    val = (2 ** int(x[1:])) * b[x]
-    total += val
-    print(x,val)
-print(total)
+xPlusY = int(xStr,2) + int(yStr,2)
+print(bin(xPlusY)[2:],"x+y")
+
+
+def f(b_,parents_):
+    
+    turns = []
+    done = set(b_.keys())
+    while any([x not in done for x in parents_]): 
+        turn = []
+        doneTurn = set()
+        for x in parents_:
+            if x in done:
+                continue
+            if all([y in done for y in parents_[x][:2]]):
+                doneTurn.add(x)
+                turn.append(x)
+        turns.append(turn)
+        done = done.union(doneTurn)
+        if len(doneTurn) == 0:
+            #print("ERROR")
+            return 0
+    
+    for turn in turns:
+        for x in turn:
+            b_[x] = OPS[parents_[x][2]](b_[parents_[x][0]],b_[parents_[x][1]])
+
+    total = 0
+    for x in sorted([x for x in b_ if x.startswith("z")]):
+        val = (2 ** int(x[1:])) * b_[x]
+        total += val
+    return total
+
+
+zSum = f({x:b[x] for x in b},parents)
+zBin = bin(zSum)[2:]
+error = bin(zSum ^ xPlusY)[2:]
+print(zBin,"z")
+print("     "+error,error.count("1"),"errors")
+
+swapCandidates = set()
+for i,x in enumerate(zBin[::-1]):
+    if x == "1":
+        zNum = ("z" if i > 9 else "z0")+str(i)
+        #print(i,x,zNum, parents[zNum])
+        swapCandidates.add(zNum)
+        queue = parents[zNum][:2][::]
+        while queue:
+            next = queue.pop(0)
+            if next in swapCandidates or next in b:
+                continue
+            swapCandidates.add(next)
+            if next not in parents:
+                continue
+            queue += parents[next][:2]
+#print(len(swapCandidates), len(b))
+swapCandidates = list(swapCandidates)
+
+swapAttempts = {}
+for j,y in enumerate(swapCandidates):
+    for i,x in enumerate(swapCandidates):
+        if i <= j:
+            continue
+        parents2 = {z:parents[z][::] for z in parents}
+        parents2y = parents2[y] 
+        parents2[y] = parents2[x]
+        parents2[x] = parents2y
+        zSum = f({z:b[z] for z in b},parents2)
+        zBin = bin(zSum)[2:]
+        error = bin(zSum ^ xPlusY)[2:]
+        if error.count("1") < 15:
+            print((" "*(len(bin(xPlusY))-2-len(error))) + error, error.count("1"),"errors",x,y)
+        swapAttempts[(x,y)] = {"error":error,"errorCount":error.count("1")}
+        
+
+        
+        
